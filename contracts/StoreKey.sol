4541a53a21719e8094dc84eb0c485f09c2666b76
yango3 <o3.yang@hotmail.com>
2021-08-09 16:56:02 +0800

    StoreKey.sol tested

--- contracts/StoreKey.sol
+++ contracts/StoreKey.sol
@@ -20,30 +20,63 @@ contract StoreKey {
         return index;
     }
 
-    function ownerSetPubkey(address addr, string memory pubkey)
+    function ownerSetPubkey(address addr, bytes memory pubkey)
         public
         returns (uint256)
     {
         require(msg.sender == owner);
+        require(pubkey.length == 65);
+        if (pubkeys[addr].length == 0) {
+            index++;
+        }
         pubkeys[addr] = bytes(pubkey);
         return 0;
+    }
+
+    // only owner of TX can set its own public key
+    function setPubkey(bytes memory pubkey) public returns (uint256) {
+        require(pubkey.length == 65);
+        // How to check pubkey valid?
+        require(msg.sender == pubkey2Address(pubkey));
+
+        if (pubkeys[msg.sender].length == 0) {
+            index++;
+        }
+        pubkeys[msg.sender] = bytes(pubkey);
+
+        return 0;
+    }
+
+    function pubkey2Address(bytes memory pubkey)
+        internal
+        pure
+        returns (address)
+    {
+        require(pubkey.length == 65);
+        bytes memory buf = new bytes(64);
+
+        for (uint256 i = 0; i < 64; i++) {
+            buf[i] = pubkey[i + 1];
+        }
+
+        return address(uint160(uint256(keccak256(buf))));
     }
 
     function getPubkey(address addr) public view returns (bytes memory) {
         return pubkeys[addr];
     }
 
-    // // only owner of TX can set its own public key
-    // function setPubkey(string memory pubkey) public returns (uint256) {
-    //     require(bytes(pubkey).length == 130);
-    //     // How to check pubkey valid?
-    //     require(address(keccak256(bytes(pubkey))) == msg.sender);
+    function setEncrypt(address addr, bytes memory encrypt)
+        public
+        returns (uint256)
+    {
+        require(msg.sender == owner);
+        require(encrypt.length == 161);
+        encrypts[addr] = bytes(encrypt);
+        return 0;
+    }
 
-    //     if (pubkeys[msg.sender] == bytes(0)) {
-    //         index++;
-    //     }
-    //     pubkeys[msg.sender] = bytes(pubkey);
-
-    //     return 0;
-    // }
+    function getEncrypt(address addr) public view returns (bytes memory) {
+        return encrypts[addr];
+    }
 }
--- test/StoreKey.js
+++ test/StoreKey.js
@@ -6,6 +6,7 @@ contract('StoreKey', (accounts) => {
         let account = web3.eth.accounts.privateKeyToAccount("73be53ae72b11b71beb398da1d9d4dd3cdadbd9b22dc6b98377917b9bbb8bfa8");
         console.log(account)
 
+
         const storeKeyInstance = await StoreKey.deployed();
 
         const index = await storeKeyInstance.getIndex();
@@ -24,20 +25,69 @@ contract('StoreKey', (accounts) => {
         console.log('accounts[1]', accounts[1]);
         console.log('accounts[2]', accounts[2])
 
-        const account1 = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+        const account1 = accounts[1];
         const account2 = accounts[2];
-        let result = await storeKeyInstance.ownerSetPubkey(account1, "aaaa");
-        console.log("owner设置")
+
+        // public key
+        const pubkey0 = "04e7341fe1c608b348f051deb090d86794e6a2380d5954e87e225bd4327c70a440e7ed3ea698262fb2da4d354c3dd7ae6eef3ad97a20b993a5351925923938659b";
+        const pubkey1 = "04d709c4fb8695d079625dc28f019fb615b752dbbfc7fc37ddaa0e2186ed100a88ffb9db5d2e0b4136350b984a83620dbeed13c488116bf4a1ced600d4b1199184"
+
+
+        let result = await storeKeyInstance.ownerSetPubkey(account1, Buffer.from(pubkey1, 'hex'));
+        console.log("owner设置 account1")
         console.log(result);
 
-        result = await storeKeyInstance.getPubkey(account1);
-        console.log("任何人都可以读取")
+        result = await storeKeyInstance.getPubkey(account1, { from: account1 });
+        console.log("任何人都可以读取 account1")
         console.log(result)
 
-        result = await storeKeyInstance.getPubkey(account2);
+        result = await storeKeyInstance.getPubkey(account2, { from: account1 });
         console.log("任何人都可以读取 account2")
         console.log(result)
 
+        const encrypBuf = "908532463f98e2294630c6109fb838ca0409c28b7219ff54c3a525d3a29fcb4aa79cbd3e1af28705d2298e98712c4fbb9179909f2dad2711f518abe96eaf53cfbf80cf11efbb72fc13bb3a4d0b3222b4f6df24a118f20c8683d37bb424e265679fd9f0a6b1a2fcb5ee6a42954ee1968e7e5f0c21b375c183f2b90a569551d45807daf8b856bc2f1b614ad7a83504be8ac07dcd5c4802def9ca65096496d1c5b258";
+
+
+        result = await storeKeyInstance.setEncrypt(account1, Buffer.from(encrypBuf, 'hex'), { from: account0 })
+        console.log(result);
+
+        result = await storeKeyInstance.getEncrypt(account1, { from: account2 })
+        console.log(result)
+
         assert.equal(owner, account0, "should equal account0")
+    })
+    it('Should test public key , address', async () => {
+        const storeKeyInstance = await StoreKey.deployed();
+        const account1 = accounts[1];
+        const account2 = accounts[2]
+
+        const pubkey1 = "04d709c4fb8695d079625dc28f019fb615b752dbbfc7fc37ddaa0e2186ed100a88ffb9db5d2e0b4136350b984a83620dbeed13c488116bf4a1ced600d4b1199184"
+        // const pubkey2 = "043d527ad7ab840562b36bb9015fb19f5ce545db075eb3cbe4ee116155346920240c417e9d4403e340d58e4f95a6707afe60c218dfe2d5ff3c0b90086323a8d0e7"
+
+        let result = await storeKeyInstance.setPubkey(Buffer.from(pubkey1, 'hex'), { from: account1 })
+        console.log(result)
+
+        result = await storeKeyInstance.getPubkey(account1);
+        console.log(result);
+
+
+        /*
+                result = await storeKeyInstance.testPubkey1({ from: account1 })
+                console.log("\ntestpublickey1:")
+                console.log(result)
+                console.log(result.toString('hex'))
+        
+                result = await storeKeyInstance.testPubkey2(Buffer.from(pubkey1, 'hex'), { from: account1 })
+                console.log("\ntestpublickey2:")
+                console.log(result)
+                console.log(result.toString('hex'))
+                
+                        result = await storeKeyInstance.testPubkey2(Buffer.from(pubkey2, 'hex'), { from: account1 })
+                        console.log("\ntestpublickey3:")
+                        console.log(result)
+                        console.log(result.toString('hex')) */
+
+        assert.equal(1, 1, "test OK")
+
     })
 });
